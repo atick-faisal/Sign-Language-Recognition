26e92f79509fffe119185689a322d582b9f56ae0
Atick Faisal <atickfaisal@gmail.com>
2022-05-21 02:19:04 +0300

    Removig scrict mode solves warning

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import "./scss/app.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    // <React.StrictMode>
+    <App />
+    // </React.StrictMode>
 );
