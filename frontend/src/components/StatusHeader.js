0ea767176300b779161cd6a0809bc6d7af1c301a
Atick Faisal <atickfaisal@gmail.com>
2022-06-01 01:58:03 +0300

    Update ui color

--- frontend/src/components/StatusHeader.js
+++ frontend/src/components/StatusHeader.js
@@ -5,17 +5,17 @@ import { FaRegHandPaper, FaFingerprint } from "react-icons/fa";
 export default function StatusHeader({ framerate, hands, fingers }) {
     return (
         <ButtonGroup>
-            <Button className="w-100" outline color="secondary">
+            <Button className="w-100" outline color="dark">
                 <FaRegHandPaper size={30} style={{ margin: "0.7rem" }} />
                 <br />
                 <h6>Number of Hands: {hands}</h6>
             </Button>
-            <Button className="w-100" outline color="secondary">
+            <Button className="w-100" outline color="dark">
                 <BsSpeedometer size={30} style={{ margin: "0.7rem" }} />
                 <br />
                 <h6>Frame Rate: {parseInt(framerate)}</h6>
             </Button>
-            <Button className="w-100" outline color="secondary">
+            <Button className="w-100" outline color="dark">
                 <FaFingerprint size={30} style={{ margin: "0.7rem" }} />
                 <br />
                 <h6>Number of Fingers: {fingers}</h6>
