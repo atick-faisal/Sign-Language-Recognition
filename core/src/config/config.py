b4678aaed927f7bf49fb229bb47aeaac6b86aa26
Atick Faisal <atickfaisal@gmail.com>
2022-06-20 05:00:53 +0300

    Train model for updated dataset

--- core/src/config/config.py
+++ core/src/config/config.py
@@ -3,7 +3,7 @@ HOLD_TIME = 2.0  # ... Time before next prediction session
 SEGMENT_LEN = 256
 IMG_LEN = 224  # 1/9 of the 224x224 image
 N_CHANNELS = 1
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.0003
 PROJECTION_LANDMARKS = ["rp", "rf0", "rf1"]
 INFERENCE_FEATURES = [
     "rpx",
