b1915466ed9cdc5c8e2ecf7b1e47ac219c317e93
Atick Faisal <atickfaisal@gmail.com>
2022-07-02 02:48:44 +0300

    Update features

--- core/src/utils/features.py
+++ core/src/utils/features.py
@@ -9,17 +9,40 @@ def extract_flxion_features(
     data: pd.DataFrame,
     segment_length: int = 150
 ) -> np.ndarray:
-    data["drf0x"] = data["rf0x"] - data["rpx"]
-    data["drf0y"] = data["rf0y"] - data["rpy"]
-    data["drf0z"] = data["rf0z"] - data["rpz"]
+    features = pd.DataFrame()
 
-    data["drf1x"] = data["rf1x"] - data["rpx"]
-    data["drf1y"] = data["rf1y"] - data["rpy"]
-    data["drf1z"] = data["rf1z"] - data["rpz"]
+    # ... Distance to thumb finger-tip
+    features["drf0"] = np.sqrt(
+        np.square(data["rf0x"] - data["rpx"]) +
+        np.square(data["rf0z"] - data["rpz"])
+    )
 
-    data = data[Config.DIST_FEATURES]
+    # ... Distance to index finger-tip
+    features["drf1"] = np.sqrt(
+        np.square(data["rf1x"] - data["rpx"]) +
+        np.square(data["rf1z"] - data["rpz"])
+    )
+
+    # ... Distance to middle finger-tip
+    features["drf2"] = np.sqrt(
+        np.square(data["rf2x"] - data["rpx"]) +
+        np.square(data["rf2z"] - data["rpz"])
+    )
+
+    # ... Thumb coordineates wrt palm (for determining orientation)
+    features["drf0x"] = data["rf0x"] - data["rpx"]
+    features["drf0z"] = data["rf0z"] - data["rpz"]
+
+    # Features v1.0
+    # features["drf0x"] = data["rf0x"] - data["rpx"]
+    # features["drf0y"] = data["rf0y"] - data["rpy"]
+    # features["drf0z"] = data["rf0z"] - data["rpz"]
+
+    # features["drf1x"] = data["rf1x"] - data["rpx"]
+    # features["drf1y"] = data["rf1y"] - data["rpy"]
+    # features["drf1z"] = data["rf1z"] - data["rpz"]
 
     # ... Min-Max scaling of distance features
-    data = (data - Config.SCALER_MIN) / Config.SCALER_RANGE
+    features = (features - Config.SCALER_MIN) / Config.SCALER_RANGE
 
-    return resample(data.to_numpy(), segment_length, axis=0)
+    return resample(features.to_numpy(), segment_length, axis=0)
